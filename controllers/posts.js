d82f083a302948a9db7d9a9e6a8488e1b01dd4fc
ricky <rjbenavides@cpp.edu>
2020-11-30 13:49:59 -0800

    added like and delete post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,5 @@
+import mongoose from "mongoose";
+
 import Post from "../models/postModel.js";
 
 export const getPosts = async (req, res) => {
@@ -21,11 +23,43 @@ export const createPost = async (req, res) => {
 };
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.hljs - params;
+  const { id: _id } = req.params;
   const post = req.body;
+
   try {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(404).send("No post with that id found");
+    }
     const updatedPost = await Post.findByIdAndUpdate(_id, post, { new: true });
     res.json(updatedPost);
+  } catch (error) {
+    res.status(404).send("No post with that id found");
+  }
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).send("No post with that id found");
+    }
+    await Post.findByIdAndRemove(id);
+    res.json({ message: "Post deleted" });
+  } catch (error) {
+    res.status(404).send("No post with that id found");
+  }
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).send("No post with that id found");
+    }
+    const post = await Post.findById(id);
+    post.likeCount = post.likeCount + 1;
+    await post.save();
+    res.json(post);
   } catch (error) {
     res.status(404).send("No post with that id found");
   }
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,16 @@
 import express from "express";
 
-import { getPosts, createPost, updatePost } from "../controllers/posts.js";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  likePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.route("/").get(getPosts).post(createPost);
-router.route("/:id").patch(updatePost);
+router.route("/:id").patch(updatePost).delete(deletePost);
+router.route("/:id/likes").patch(likePost);
 export default router;
